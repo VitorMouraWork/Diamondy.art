36330757f0d86a9947a687de5e61ccc545758de8
Vitor D <12100471@aluno.cotemig.com.br>
2023-06-04 22:33:11 -0300

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Newsflash from '@/components/Newsflash';
 import '../styles/globals.css'
 import Header from '/components/Header';
+import Head from 'next/head';
 
 export const metadata = {
   title: 'Diamondy.art',
@@ -10,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body><Header/>{children}<Newsflash/></body>
     </html>
   )
