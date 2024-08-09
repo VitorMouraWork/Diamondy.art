5650e175350e2bea845efd4a8bfbdc518ca32243
Vitor D. <vitormourawork@gmail.com>
2024-08-09 12:21:25 -0300

    Update Characters.jsx

--- app/otom/otom components/Characters.jsx
+++ app/otom/otom components/Characters.jsx
@@ -5,33 +5,33 @@ import Image from 'next/image';
 
 // Sample array of character image paths (replace with actual paths)
 const characters = [
-  <img src={'/characters/frame98.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame99.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame100.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame101.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame102.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame103.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame104.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame105.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame106.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame107.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame108.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame109.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame110.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame111.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame112.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame113.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame114.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame115.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame116.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame117.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame118.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame119.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame120.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame121.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame122.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame123.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
-  <img src={'/characters/frame124.png'} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>,
+  '/characters/Frame98.png',
+  '/characters/Frame99.png',
+  '/characters/Frame100.png',
+  '/characters/Frame101.png',
+  '/characters/Frame102.png',
+  '/characters/Frame103.png',
+  '/characters/Frame104.png',
+  '/characters/Frame105.png',
+  '/characters/Frame106.png',
+  '/characters/Frame107.png',
+  '/characters/Frame108.png',
+  '/characters/Frame109.png',
+  '/characters/Frame110.png',
+  '/characters/Frame111.png',
+  '/characters/Frame112.png',
+  '/characters/Frame113.png',
+  '/characters/Frame114.png',
+  '/characters/Frame115.png',
+  '/characters/Frame116.png',
+  '/characters/Frame117.png',
+  '/characters/Frame118.png',
+  '/characters/Frame119.png',
+  '/characters/Frame120.png',
+  '/characters/Frame121.png',
+  '/characters/Frame122.png',
+  '/characters/Frame123.png',
+  '/characters/Frame124.png',
 ];
 
 const usedCharacters = new Set(); // To keep track of used characters
@@ -59,7 +59,9 @@ const Characters = () => {
   }, []);
 
   return (
-    character
+    character && (
+      <img src={character} className='aspect-auto max-w-52 max-h-52 absolute z-10 gelatine'/>
+    )
   );
 };
 
