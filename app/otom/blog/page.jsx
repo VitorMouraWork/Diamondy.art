53bfa16fca4c8e1c7276fce9b170fcb88a10e689
Vitor D. <vitormourawork@gmail.com>
2024-08-10 20:11:02 -0300

    blog page fix

--- app/otom/blog/page.jsx
+++ app/otom/blog/page.jsx
@@ -1,15 +1,8 @@
 "use client";
 
-import '../otom.css';
-import Link from 'next/link';
-import Image from 'next/image';
-import Front from "./Contents/wikiHome.mdx";
-import WikiHeader from './wiki components/WikiHeader';
-
-const OtomWiki = () => {
+const OtomBlog = () => {
   return (
     <>
-    <WikiHeader/>
       <section className="w-full h-full z-10 flex flex-col items-center justify-center">     
         <div className='flex flex-col justify-center items-center w-full py-40 space-y-3 bgblur'>
           <div className='duration-200 flex flex-col ease-out bg-white dark:bg-slate-900 drop-shadow-md p-5 rounded-3xl w-[80rem] max-w-[80rem] space-y-5'>
@@ -22,4 +15,4 @@ const OtomWiki = () => {
   )
 }
 
-export default OtomWiki;
+export default OtomBlog;
