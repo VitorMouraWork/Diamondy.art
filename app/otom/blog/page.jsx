af1c4345cec8bf31ba042d789b9026b881237256
Vitor D. <vitormourawork@gmail.com>
2024-08-12 12:27:29 -0300

    blog og image

--- app/otom/blog/page.jsx
+++ app/otom/blog/page.jsx
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import OtomHeader from "../otom components/OtomHeader";
+import Image from 'next/image';
 
 // Define the path to the Blog contents directory
 const CONTENT_DIR = path.join(process.cwd(), 'app', 'Contents', 'Blog');
@@ -31,7 +32,7 @@ const OtomBlog = async () => {
       <section className="w-full h-full z-10 flex flex-col items-center justify-center">     
         <div className='flex flex-col justify-center items-center w-full py-40 space-y-3 bgblur'>
           <div className='duration-200 flex flex-col ease-out bg-white dark:bg-slate-900 drop-shadow-md p-5 rounded-3xl w-[80rem] max-w-[80rem] space-y-5'>
-            <h1 className='otomtext text-dyblack dark:text-white text-3xl'>BLOG</h1>
+              <Image src="/assets/icons/otom/blog.svg" width={250} height={250}/>
             <hr className='dark:opacity-20'></hr>
             <div>
               {/* Loop through the posts and display them */}
